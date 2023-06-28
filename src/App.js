91ddf56424e4b90df781b26178bbf79a3cb8b19a
unknown <shubhamvaishnav.dev@gmail.com>
2023-06-28 22:57:06 +0530

    version 0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import i18n from 'i18next';
+import { useTranslation, initReactI18next } from 'react-i18next';
 
-function App() {
+import English from './languages/English.json';
+import French from './languages/French.json';
+
+
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: English
+      },
+      fr: {
+        translation: French
+      },
+    },
+    lang: "en", // remove this during use of languge detector
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+const App = () => {
+
+  const { t } = useTranslation();
+
+  const changeLang = (lang) => {
+    i18n.changeLanguage(lang); //use to set diffrent language
+
+    //localstorage set key and selected language value in storage
+    localStorage.setItem('language', lang)
+  }
+
+  useEffect(() => {
+    const currentLang = localStorage.getItem('language') // get value of stored language
+    i18n.changeLanguage(currentLang); // set language value
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-[100vh] w-[100vw] flex flex-col justify-center items-center px-4 bg-sky-500'>
+      <div className='h-[60%] w-[70%] flex flex-col justify-center items-center bg-gray-200'>
+        <p className='text-center text-3xl font-bold'>{t('intro')}</p>
+        <div className='flex gap-8 mt-4'>
+          <button onClick={() => changeLang('en')} className='px-4 py-2 bg-green-500' >english</button>
+          <button onClick={() => changeLang('fr')} className='px-4 py-2 bg-green-500' >french</button>
+          <button onClick={() => localStorage.clear()} className='px-4 py-2 bg-green-500' >reset</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
